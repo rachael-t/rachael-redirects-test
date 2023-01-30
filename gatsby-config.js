e19c81e335cf6ee9d313db952495f704557cdaff
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2023-01-30 14:39:53 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Rachael Redirects Test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
