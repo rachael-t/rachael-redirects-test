b9dd805e91a12036e221cbc0701652d519c0fb4e
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2023-01-30 14:48:28 -0700

    Add gatsby-node and redirect

---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: `/test/*`,
+    toPath: `https://www.gatsbyjs.com/*`,
+    statusCode: 200,
+  })
+}
