2b7901c9e6a5ba83c83cddc0defa21236434e328
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2023-02-01 11:36:02 -0700

    Add caching code for different reproduction

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-const redirects = require("./redirects.json") 
+// const redirects = require("./redirects.json") 
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createRedirect } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+  // const { createRedirect } = actions
 
   // this doesn't seem to work, because the page /hello/ exists
 //   createRedirect({
@@ -10,16 +10,32 @@ exports.createPages = async ({ graphql, actions }) => {
 //     statusCode: 200,
 //   })
 
-  redirects.forEach(redirect =>
-    createRedirect({
-      fromPath: redirect.fromPath,
-      toPath: redirect.toPath,
-    })
-  )
+  // redirects.forEach(redirect =>
+  //   createRedirect({
+  //     fromPath: redirect.fromPath,
+  //     toPath: redirect.toPath,
+  //   })
+  // )
 
-    createRedirect({
-    fromPath: `/rachael/`,
-    toPath: `/hello/`,
-    statusCode: 200,
-  })
+  //   createRedirect({
+  //   fromPath: `/rachael/`,
+  //   toPath: `/hello/`,
+  //   statusCode: 200,
+  // })
+
+
+// }
+
+exports.onPostBuild = async function ({ cache }) {
+  const cacheKey = "test-key"
+  let obj = await cache.get(cacheKey)
+
+  if (!obj) {
+    console.log(`No cache object found with key "${cacheKey}." Caching key "${cacheKey}" now.`)
+    obj = { created: Date.now() }
+    await cache.set(cacheKey, obj)
+  } else {
+    console.log(`Found cache object with key ${cacheKey} and created value ${obj.created}!`)
+  }
 }
+
